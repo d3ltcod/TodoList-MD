7a4d67d43dcff13bdcfdd87dee0762322e753913
AlvaradoAdam15 <adamalvarado@iesebre.com>
2016-03-12 11:18:37 +0100

    Separated adapter in other class

---
+++ app/src/main/java/adam/dam2/iesebre/com/todolistmd/Adapters/ListAdapter.java
@@ -0,0 +1,70 @@
+package adam.dam2.iesebre.com.todolistmd.Adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import java.util.List;
+import adam.dam2.iesebre.com.todolistmd.ItemDetailActivity;
+import adam.dam2.iesebre.com.todolistmd.ItemDetailFragment;
+import adam.dam2.iesebre.com.todolistmd.ItemListActivity;
+import adam.dam2.iesebre.com.todolistmd.Models.TodoListViewHolder;
+import adam.dam2.iesebre.com.todolistmd.R;
+import adam.dam2.iesebre.com.todolistmd.dummy.DummyContent;
+
+/**
+ * Created by adam on 12/03/16.
+ */
+public class ListAdapter extends RecyclerView.Adapter<TodoListViewHolder> {
+    private final List<DummyContent.DummyItem> mValues;
+    private final FragmentActivity activity;
+
+    public ListAdapter(List<DummyContent.DummyItem> items, Activity activity) {
+        mValues = items;
+        this.activity =(FragmentActivity) activity;
+    }
+
+    @Override
+    public TodoListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list_content, parent, false);
+        return new TodoListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final TodoListViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mContentView.setText(mValues.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ItemListActivity.mTwoPane) {
+                    Bundle arguments = new Bundle();
+                    arguments.putString(ItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+                    ItemDetailFragment fragment = new ItemDetailFragment();
+                    fragment.setArguments(arguments);
+                    activity.getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.item_detail_container, fragment)
+                            .commit();
+                } else {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, ItemDetailActivity.class);
+                    intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+
+                    context.startActivity(intent);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+}
---
+++ app/src/main/java/adam/dam2/iesebre/com/todolistmd/Models/TodoListViewHolder.java
@@ -0,0 +1,31 @@
+package adam.dam2.iesebre.com.todolistmd.Models;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import adam.dam2.iesebre.com.todolistmd.R;
+import adam.dam2.iesebre.com.todolistmd.dummy.DummyContent;
+
+/**
+ * Created by adam on 12/03/16.
+ */
+public class TodoListViewHolder extends RecyclerView.ViewHolder {
+
+    public final View mView;
+    public final TextView mContentView;
+    public final CheckBox mDoneView;
+    public DummyContent.DummyItem mItem;
+
+    public TodoListViewHolder(View view) {
+        super(view);
+        mView = view;
+        mContentView = (TextView) view.findViewById(R.id.todoListItemText);
+        mDoneView = (CheckBox) view.findViewById(R.id.checkboxDone);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " '" + mDoneView.isChecked() + mContentView.getText() + "'";
+    }
+}
